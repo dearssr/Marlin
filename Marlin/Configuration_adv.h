cda7df68a7a945feafc05284e17bf6913c83c80a
Sergey Rakitin <dear.ssr@gmail.com>
2021-01-23 23:08:24 +0200

    Configuration changes (13.4 init)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2383,7 +2383,7 @@
   #if AXIS_IS_TMC(Z2)
     #define Z2_CURRENT      800
     #define Z2_CURRENT_HOME Z2_CURRENT
-    #define Z2_MICROSTEPS    1
+    #define Z2_MICROSTEPS    16
     #define Z2_RSENSE         0.11
     #define Z2_CHAIN_POS     -1
     //#define Z2_INTERPOLATE true
