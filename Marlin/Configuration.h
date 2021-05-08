15d2c59cd81b4b45d245490214db0e077a5a19e6
Sergey Rakitin <dear.ssr@gmail.com>
2021-05-08 13:28:10 +0300

    Configuration changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -862,7 +862,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 400, 1680 } // { 160, 160, 400, 420 } // 142.05 // 427.50 // 94.86 (direct) // { 40, 40, 200, 70.43 } //{ 160, 160, 800, 281.72 } // 140.86 //{ 100, 100, 400, 95 } //{ 200, 200, 800, 190 } //{ 100, 100, 400, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 420 } // { 320, 320, 400, 1680 } // { 160, 160, 400, 420 } // 142.05 // 427.50 // 94.86 (direct) // { 40, 40, 200, 70.43 } //{ 160, 160, 800, 281.72 } // 140.86 //{ 100, 100, 400, 95 } //{ 200, 200, 800, 190 } //{ 100, 100, 400, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1116,14 +1116,14 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 36, 0, -1.8 } // { 22, -8, -4 } // { -30, -18, -1.9 } // { -5, 57, -7 } //{ 25, -8, -5 }
+#define NOZZLE_TO_PROBE_OFFSET { 36, 0, -2 } // { 22, -8, -4 } // { -30, -18, -1.9 } // { -5, 57, -7 } //{ 25, -8, -5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 //#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED 6000 // (133*60)
+#define XY_PROBE_FEEDRATE 6000 // (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (4*60)
